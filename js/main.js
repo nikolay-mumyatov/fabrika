8d8444b33cec00448bd694e1840340a58b9e023b
nikolay-mumyatov <nikolay.mumyatov@yandex.ru>
2020-03-27 00:12:32 +0300

    Версика блока "Каталог" + адаптив.

--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
-// Появление меню в десктопной версии.
+// Появление меню.
 
 $(function () {
     var menuBtn = $('.navbar__menu-button'),
@@ -6,5 +6,17 @@ $(function () {
 
     menuBtn.on('click', function(){
         menu.toggleClass('menu_active');
+    });
+});
+
+
+// Появление кнопок в карточке каталога.
+
+$(function () {
+    var card = $('.catalog__card'),
+        card_buttons = $('.catalog-card__buttons');
+        
+    card.on('click', function(){
+        $(this).find(card_buttons).toggleClass('visible');
     });
 });
